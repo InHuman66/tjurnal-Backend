ee1834eaffe9f24006dc728a9862a7b0b75e7a46
InHuman66 <olexandr.kk@gmail.com>
2021-10-23 09:32:38 +0300

    create user endpoint, controller, dto

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail } from 'class-validator';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullName: string;
+
+  @Column({ nullable: true })
+  password?: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+}
